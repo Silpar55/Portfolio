c158ee339106181768f466d73335ee992a318a16
Silpar55 <asilva-juarez@myseneca.ca>
2023-07-03 01:08:00 -0500

    Skill page with Swipe.js

--- pages/components/skills/Slide.tsx
+++ pages/components/skills/Slide.tsx
@@ -10,17 +10,17 @@ type Props = {
 const Slide = ({ imgUrl, imgUrl2, title, text }: Props) => {
   return (
     <section
-      className={`flex flex-col xl:flex-row justify-center items-center h-full gap-5 px-5  md:px-20 `}
+      className={`flex flex-col xl:flex-row justify-center items-center h-full gap-10 px-5  md:px-20 `}
     >
       <div className="text-center xl:basis-2/3">
         <h1 className="font-bold text-5xl md:text-6xl xl:text-7xl text-cyan-600">
           {title}
         </h1>
-        <p className="text-xl md:text-2xl xl:text-3xl px-5 mt-5 font-semibold">
+        <p className="text-xl sm:text-2xl xl:text-3xl px-5 mt-5 font-semibold">
           {text}
         </p>
       </div>
-      <div className="flex-col xl:basis-1/3 sm:px-5">
+      <div className={`flex flex-col xl:basis-1/3 sm:px-5`}>
         {imgUrl && (
           <Image
             src={imgUrl}
@@ -29,8 +29,8 @@ const Slide = ({ imgUrl, imgUrl2, title, text }: Props) => {
             height={400}
             loading="eager"
             className={`object-cover w-auto  ${
-              imgUrl2 ? "h-32 " : " h-44"
-            } xl:w-full xl:h-auto m-auto md:h-56`}
+              imgUrl2 ? "h-32 " : "h-40"
+            } xl:w-full xl:h-auto m-auto sm:h-56`}
           />
         )}
         {imgUrl2 && (
